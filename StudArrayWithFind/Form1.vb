f3474b3b1fae46f00f18211b6645b137c12c39d1
Finnegan O'Connor <Finnegan.OConnor@education.nsw.gov.au>
2020-10-28 11:44:34 +1100

    New

--- StudArrayWithFind/Form1.vb
+++ StudArrayWithFind/Form1.vb
@@ -90,4 +90,8 @@
     Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
 
     End Sub
+
+    Private Sub txtAvMk_TextChanged(sender As Object, e As EventArgs) Handles txtAvMk.TextChanged
+
+    End Sub
 End Class
